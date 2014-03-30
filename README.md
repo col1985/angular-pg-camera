e80dcf601d10ee28dad2a7b087d99868bea32be2
Colum Bennett <col1985@gmail.com>
2014-03-30 18:24:56 +0100

    Refactored module wrapper, changed file structure, implementing browserr support

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-Angular Service Module wrapping PhoneGap Camera Native API v3.0.
+##Angular Service Module wrapping PhoneGap Camera Native API v3.0.
+==================================================================================================================
 
-Currently configured for iOS.
+So I have decided to re-think my approach to creating this wrapped Camera API.
 
-I will update detail of module and properties later today.
+My aim to write a fully functional Camera component using Angular's module Schema.
 
-Untested at present.
++ Currently adding support for in browser development using getUserMedia API
++ use of $q and promises to handle component loading
++ using $document to bind deviceready event to $digest cycle
++ creaing component controller and directive to implement component 
--- camera.module.js
+++ camera.module.js
@@ -3,97 +3,113 @@
  * @description: angularJS service module wrapping Phonegaps Native Camera API, further info see README.md.
  */
 
-'use strict'
+'use strict';
 
-angular.module('camera.services')
-    .factory("phonegapCamera", ['$rootScope', '$q', 'env',
-        function($rootScope, $q, env) {
-            console.debug("phonegapCamera", env);
+var cam = angular.module('Camera', []);
 
-            return {
-                getPicture: function(opts) {
-
-                    // init q
-                    var deferred = $q.defer();
+cam.factory("phonegapCamera", ['$window', '$document', '$rootScope', '$q',
+    function($window, $document, $rootScope, $q) {
+        console.debug("phonegapCamera", env);
 
 
+        /**
+         * check for existence of getUserMedia API
+         */
+
+        function hasGetUserMedia() {
+            return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia || navigator.msGetUserMedia);
+        }
+
+        return {
+            getPicture: function(opts) {
+
+                // init q
+                var deferred = $q.defer();
+
+                /**
+                 * check for browser, implement fallback for
+                 * debugging in browser.
+                 */
+                if (hasGetUserMedia() && browser) {
+
+                    navigator.getUserMedia = navigator.getUserMedia ||
+                        navigator.webkitGetUserMedia ||
+                        navigator.mozGetUserMedia ||
+                        navigator.msGetUserMedia;
+
+                    // TODO: Investigate using getUserMedia API
+                    // for desktop development
+
+
+                } else {
+
+                    var defaultOpts = {
+                        quality: 75,
+                        destinationType: Camera.DestinationType.DATA_URL,
+                        allowEdit: true,
+                        targetWidth: 75,
+                        targetHeight: 75
+                    };
 
                     /**
-                     * check for browser, implement fallback for
-                     * debugging in browser.
+                     * replace defaultOpts with user defined opts
                      */
-                    if (env.browser) {
-
-                        /**
-                         * create element, but dont append to dom
-                         */
-
-                        var fileEl = document.createElement('input');
-                        fileEl = setAttribute('type', 'file');
-
-                        fileEl.onchnage = function() {
-                            var file = fileEl.files[0];
-                            var reader = new FileReader();
-                            reader.readAsDataURL(file);
-                            reader.onLoadEnd = function() {
-                                $rootScope.$apply(function() {
-                                    // strip beginning from string
-                                    var encodedData = reader.result.replace(/data:image\/jpeg;base64,/, '');
-                                    deferred.resolve(encodedData);
-                                });
-                            }
-                        };
-
-                        fileEl.click();
-                    } else {
-
-                        var defaultOpts = {
-                            quality: 75,
-                            destinationType: Camera.DestinationType.DATA_URL,
-                            allowEdit: true,
-                            targetWidth: 75,
-                            targetHeight: 75
-                        };
-
-                        /**
-                         * replace defaultOpts with user defined opts
-                         */
-                        if (opts) {
-                            opts = angular.extend(defaultOpts, opts);
-                        }
-
-                        /**
-                         * handle Image capture success
-                         */
-                        var success = function(imageData) {
-                            console.debug(imageData);
-                            $rootScope.$apply(function() {
-                                deferred.resolve(imageData);
-                            });
-                        };
-
-                        /**
-                         * handle Image capture fail
-                         */
-                        var failed = function(message) {
-                            console.debug(message);
-                            $rootScope.$apply(function() {
-                                deferred.reject(message);
-                            })
-                        };
-
-                        /**
-                         * Call camera via Cordova
-                         */
-                        navigator.camera.getPicture(success, failed, opts);
-
-                        /**
-                         * return promise
-                         */
-                        return deferred.promise;
+                    if (opts) {
+                        opts = angular.extend(defaultOpts, opts);
                     }
 
+                    /**
+                     * handle Image capture success
+                     */
+                    var success = function(imageData) {
+                        console.debug(imageData);
+                        $rootScope.$apply(function() {
+                            deferred.resolve(imageData);
+                        });
+                    };
+
+                    /**
+                     * handle Image capture fail
+                     */
+                    var failed = function(message) {
+                        console.debug(message);
+                        $rootScope.$apply(function() {
+                            deferred.reject(message);
+                        })
+                    };
+
+                    /**
+                     * Call camera via Cordova on deviceready
+                     */
+                    $document.bind('deviceready', function() {
+                        navigator.camera.getPicture(success, failed, opts);
+                    })
+
+                    /**
+                     * return promise
+                     */
+                    return deferred.promise;
                 }
-            };
-        }
-    ]);
+
+            }
+        };
+    }
+]);
+
+cam.directive('pgCamera', ['$scope',
+    function($scope) {
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: true,
+            controller: 'CameraCtrl'
+            link: function($scope, elem, attrs, ctrl) {
+                console.debug("pgCamera directive");
+
+
+            },
+            templateUrl: 'components/camera.html',
+        };
+    }
+]);
